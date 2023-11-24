c8a27aeea51f1ac8486c6229a8d8aadda8df3dc9
SStudzinska <sstudzinska01@gmail.com>
2023-11-24 12:55:45 +0100

    note inserting logic (+a list of notes!)

--- app/src/main/java/com/example/androidapp/database/model/Note.kt
+++ app/src/main/java/com/example/androidapp/database/model/Note.kt
@@ -1,6 +1,19 @@
 package com.example.androidapp.database.model
 
-data class Note(
-    var noteTitle: String = "",
-    var content: String = "",
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Notes(
+    @PrimaryKey(autoGenerate = true)
+    val noteId: Long?=0,
+    @ColumnInfo(name="title")
+    val noteTitle: String = "",
+    @ColumnInfo(name="content")
+    val content: String = "",
+    val date:String = "",
+    val pinned: Boolean = false
+
 )
