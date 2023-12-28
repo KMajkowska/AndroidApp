e9c97805f5a7913b0763dd63cde3b94119bf8f30
karol <karolina.majkowska@o2.pl>
2023-12-28 18:46:47 +0100

    UNICOOORN

--- app/src/main/java/com/example/androidapp/ScreenUtil.kt
+++ app/src/main/java/com/example/androidapp/ScreenUtil.kt
@@ -28,8 +28,8 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -37,19 +37,48 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.androidapp.settings.SettingsRepository
+import com.example.androidapp.settings.SettingsViewModel
+import com.example.androidapp.settings.SettingsViewModelFactory
 
 @Composable
 fun AddBackgroundToComposables(vararg composables: @Composable () -> Unit) {
+    val mSettingsViewModel: SettingsViewModel = viewModel(
+        factory = SettingsViewModelFactory(SettingsRepository(LocalContext.current))
+    )
+    val isUniqrnTheme by mSettingsViewModel.isUniqrnModeEnabled.observeAsState(false)
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(Color.hsv(242F, 0.729F, 1F), Color.hsv(209F, 0.683F, 0.953F)),
-                    startY = 0.0f,
-                    endY = Float.POSITIVE_INFINITY
-                )
+                brush = if (isUniqrnTheme) {
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.hsv(0F, 1F, 1F),
+                            Color.hsv(30F, 1F, 1F),
+                            Color.hsv(60F, 1F, 1F),
+                            Color.hsv(120F, 1F, 1F),
+                            Color.hsv(180F, 1F, 1F),
+                            Color.hsv(240F, 1F, 1F),
+                            Color.hsv(270F, 1F, 1F),
+                            Color.hsv(300F, 1F, 1F)
+                        ),
+                        startY = 0.0f,
+                        endY = Float.POSITIVE_INFINITY
+                    )
+                } else {
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.hsv(242F, 0.729F, 1F),
+                            Color.hsv(209F, 0.683F, 0.953F)
+                        ),
+                        startY = 0.0f,
+                        endY = Float.POSITIVE_INFINITY
+                    )
+                }
             )
     ) {
         Column(
